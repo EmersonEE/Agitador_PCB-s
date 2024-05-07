2480a43a20395c704439f698a25ce9fd148d99f7
EmersonEE <perezemerson85@gmail.com>
2024-05-07 16:17:37 -0600

    Agregando mensajes de Bienvenida

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,54 @@
 #define ANCHO 128
 #define ALTO 64
 
-#define OLED_RESET 4
-// oled Objeto
-Adafruit_SSD1306 oled(ANCHO,ALTO, &Wire,OLED_RESET);
+uint8_t push1 = 8;  // INPUT
+uint8_t push2 = 12; // INPUT
+uint8_t push3 = 13; // INPUT
+uint8_t push4 = 7;  // INPUT
 
+#define DISPLAY_RESET 4
+// display Objeto
+Adafruit_SSD1306 lcd(ANCHO, ALTO, &Wire, DISPLAY_RESET);
 
 void setup()
 {
     Wire.begin();
-    oled.begin(SSD1306_SWITCHCAPVCC,0x3C);
-    
+    lcd.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 }
 
+void Bienvenida()
+{
+
+    lcd.clearDisplay();
+    lcd.drawRect(10, 20, 110, 40, WHITE); // dibuja rectangulo
+    lcd.setCursor(18, 34);                // ubica cursor en coordenadas 28,34
+    lcd.setTextSize(2);                   // establece tamano de texto en 2
+    lcd.setTextColor(WHITE);              // establece color al unico disponible (pantalla monocromo)
+    lcd.print("AGITADOR");                // escribe texto
+    delay(1000);
+    lcd.display();
+    lcd.clearDisplay();
+    lcd.drawRect(10, 20, 110, 40, WHITE); // dibuja rectangulo
+    lcd.setCursor(18, 34);                // ubica cursor en coordenadas 28,34
+    lcd.setTextSize(2);                   // establece tamano de texto en 2
+    lcd.setTextColor(WHITE);              // establece color al unico disponible (pantalla monocromo)
+    lcd.print("EMER :)");                 // escribe texto
+    delay(1000);
+    lcd.display(); // muestra en pantalla todo lo establecido anteriormente
+}
 void loop()
 {
+    Bienvenida();
+    lcd.clearDisplay();
+    lcd.setCursor(20,4);
+    lcd.setTextSize(1);
+    lcd.print("Eliga el Tiempo");
+    lcd.setCursor(18,20);
+    lcd.setTextSize(1);
+    lcd.print("A Agitar el PCB");
+    lcd.setCursor(0,40);
+    lcd.setTextSize(2);
+    lcd.print("MIN: ");
+    lcd.display();
+    delay(1000);
 }
